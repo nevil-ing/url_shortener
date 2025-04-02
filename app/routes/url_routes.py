a1b0e6d4fe20a605658a1a5177396526d6353b22
Nevil-ing <nevilpurpp12@gmail.com>
2025-04-03 01:09:31 +0300

    defined routes

--- app/routes/url_routes.py
+++ app/routes/url_routes.py
@@ -1,4 +1,6 @@
 from pydantic import Basemodel, HttpUrl
+from fastapi import APIRouter
+
 
 class URLBase(Basemodel):
     long_url: HttpUrl
@@ -8,4 +10,23 @@ class URLResponse(URLBase):
 
 BASE_URL =""    
 
+router = APIRouter()
+
+@router.post()
+async def create_url():
+    pass
+
+
+@router.get()
+async def get_url():
+    pass
+
+
+@router.put()
+async def update_url():
+    pass
+
+@router.delete()
+async def remove_url():
+    pass
 
